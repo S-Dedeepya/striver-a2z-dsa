d04b2e0ed2d48d221c61996400ebfbb80aa3d066
S-Dedeepya <dedeepya3116@gmail.com>
2026-05-26 13:25:58 +0530

    Patterns (7-12)

--- BASICS/Patterns_22.java
+++ BASICS/Patterns_22.java
@@ -1,6 +1,6 @@
 public class Patterns_22{
     public static void main(String[] args) {
-        int n=5;
+        int n=4;
         // Pattern 1
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -39,6 +39,98 @@ public class Patterns_22{
         //pattern 6
         for(int i=n;i>0;i--){
             for(int j=0;j<i;j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+        //pattern 7
+        for(int i=0;i<n;i++){
+            for(int j=n;(j-i-1)>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //pattern 8
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+            for(int j=n-i-1;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //pattern 9
+        for(int i=0;i<n;i++){
+            for(int j=n;(j-i-1)>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+            for(int j=n-i-1;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //pattern 10
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--){
+            for(int j=i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //pattern 11
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i && i%2==0;j++){
+                System.out.print((j+1)% 2);
+            }
+            for(int j=0;j<=i && i%2 !=0;j++){
+                System.out.print((j)% 2);
+            }
+            System.out.println();
+        }
+        //pattern 12
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(j+1);
+            }
+            for(int j=n-i-1;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=0;j--){
                 System.out.print(j+1);
             }
             System.out.println();
