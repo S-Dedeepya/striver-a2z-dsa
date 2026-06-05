b6e00991f10c90ca4ed55bdb6bb1aad48c27b46a
DEDEEPYA S <dedeepya3116@gmail.com>
2026-06-05 19:15:30 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (11.14%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxPower(String s) {
+        int max=0;
+        int c=1;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)==s.charAt(i+1)){
+                c++;
+            }else{
+                max=Math.max(max,c);
+                c=1;
+            }
+        }
+        return Math.max(max,c);
+    }
+}
