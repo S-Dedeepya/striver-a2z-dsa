1302dcf24a0ac4cc04e5343a35abcd50b8baecaf
DEDEEPYA S <dedeepya3116@gmail.com>
2026-06-16 22:11:21 +0530

    Time: 19 ms (40.57%) | Memory: 82.8 MB (11.47%) - LeetSync

---
+++ Leetcode/1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minDays(int[] arr, int m, int k) {
+        if((long)m*k > arr.length) return -1;
+        int low=Integer.MAX_VALUE;
+        int high=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            low=Math.min(low,arr[i]);
+            high=Math.max(arr[i],high);
+        }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int ans=min_days(arr,m,k,mid);
+            if(ans>=m){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+    public static int min_days(int[] arr,int m,int k,int mid){
+        int count=0;
+        int res=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=mid){
+                count++;
+            }else{
+                res+=count/k;
+                count=0;
+            }
+        }
+        res+=count/k;
+        return res;
+    }
+}
