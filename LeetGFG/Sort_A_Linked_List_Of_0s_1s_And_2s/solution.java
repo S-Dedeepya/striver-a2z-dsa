571343bbb0556278b3e3254585d3f2b6d2c5c9d8
DEDEEPYA S <dedeepya3116@gmail.com>
2026-07-22 22:07:36 +0530

    Update solution.java

--- LeetGFG/Sort_A_Linked_List_Of_0s_1s_And_2s/solution.java
+++ LeetGFG/Sort_A_Linked_List_Of_0s_1s_And_2s/solution.java
@@ -1,31 +1,54 @@
 class Solution {
-  public:
-    Node* segregate(Node* head) {
-        // code here
-        vector<Node*>t(3, nullptr);
-
-        for( int i = 0 ; i < 3 ; i++ )
-        t[i] = new Node(i);
-        
-        auto th = t ;
-        
-        auto it = head ;
-        
-        while( it ) 
-        {
-            int v = it -> data ;
-            
-            t[v]->next = it ;
-            t[v] = it ;
-            
-            it = it->next ;
-            t[v]->next = nullptr ;
+    public Node segregate(Node head) {
+        Node temp=head;
+        Node head1=null;
+        Node temp1=null;
+        int zeroes=0;
+        int ones=0;
+        int twos=0;
+        while(temp!=null){
+            if(temp.data==0){
+                zeroes++;
+            }else if(temp.data==1){
+                ones++;
+            }else{
+                twos++;
+            }
+            temp=temp.next;
         }
-        
-        t[1]->next = th[2]->next ;
-        t[0]->next = th[1]->next ;
-        
-        return th[0]->next ; 
-        
+        while(zeroes!=0){
+            Node newnode=new Node(0);
+            if(head1==null){
+                head1=newnode;
+                temp1=head1;
+            }else{
+                temp1.next=newnode;
+                temp1=temp1.next;
+            }
+            zeroes--;
+        }
+        while(ones!=0){
+            Node newnode=new Node(1);
+            if(head1==null){
+                head1=newnode;
+                temp1=head1;
+            }else{
+                temp1.next=newnode;
+                temp1=temp1.next;
+            }
+            ones--;
+        }
+        while(twos!=0){
+            Node newnode=new Node(2);
+            if(head1==null){
+                head1=newnode;
+                temp1=head1;
+            }else{
+                temp1.next=newnode;
+                temp1=temp1.next;
+            }
+            twos--;
+        }
+        return head1;
     }
-};
+}
